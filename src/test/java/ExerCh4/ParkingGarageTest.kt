0c0d056e060f54cf2236f01deeffeef3faeb99a7
Christian John Aparejado <christian.john.aparejado@oocl.com>
2019-06-07 11:19:21 +0800

    more practice

---
+++ src/test/java/ExerCh4/ParkingGarageTest.kt
@@ -0,0 +1,22 @@
+package ExerCh4
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class ParkingGarageTest{
+    val parkingGarage = ParkingGarage()
+    @Test
+    internal fun test_getParkingCharge_when_5_hours_then_3_dollars() {
+        assertEquals(3.00f, parkingGarage.getParkingCharge(5))
+    }
+
+    @Test
+    internal fun test_getParkingCharge_when_3_hours_then_return_2_dollars() {
+        assertEquals(2.00f, parkingGarage.getParkingCharge(3))
+    }
+
+    @Test
+    internal fun test_getParkingCharge_when_1_hour_then_return_2_dollars() {
+        assertEquals(2.00f, parkingGarage.getParkingCharge(1))
+    }
+}
